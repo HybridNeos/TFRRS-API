6b356d4b7f73a53ecfbc36a54f56853907aa915f
HybridNeos <mark3198@gmail.com>
2019-08-17 21:38:07 -0400

    Restructued into a class, added tests
    The TfrrsApi is now a class which accepts arguments for the athlete. Added in this commit is a test file making sure urls are formed properly and that HTML is pulled

---
+++ Tests.py
@@ -0,0 +1,31 @@
+from TfrrsApi import TfrrsApi
+import unittest
+
+class TestTfrrsApi(unittest.TestCase):
+
+    #With invalid arguments should raise an exception
+    def test_urlException(self):
+        try:
+            Test1 = TfrrsApi("60924888", "Nowhere", "No one")
+            self.assertEqual(1, 2)
+        except:
+            self.assertEqual(1, 1)
+
+    #With all arguments check for correct url, no exception, and HTML loaded
+    def test_urlAllArguments(self):
+        try:
+            Test2 = TfrrsApi("6092422", "RPI", "Mark Shapiro")
+            self.assertEqual(Test2.url, "https://www.tfrrs.org/athletes/6092422/RPI/Mark_Shapiro.html")
+            self.assertTrue(Test2.HTML)
+        except:
+            self.assertEqual(1, 2)
+
+    def test_urlJustId(self):
+        try:
+            Test3 = TfrrsApi("6092422")
+            self.assertTrue(Test3.HTML)
+        except:
+            self.assertEqual(1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
--- TfrrsApi.py
+++ TfrrsApi.py
@@ -1,72 +1,91 @@
 import pandas as pd
 import requests
-import re
 from numpy import arange
 from bs4 import BeautifulSoup
 
-def main():
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.102 Safari/537.36"}
-    response = requests.get("https://www.tfrrs.org/athletes/6092422/RPI/Mark_Shapiro.html", headers=headers)
-    
-    if response.status_code < 400 and response.status_code >= 200:
-        HTML = response.text.replace("colspan=\"100%\"", "colspan=\"3\"")
+class TfrrsApi:
+    def __init__(self, ID, school="", name=""):
+        self.url = "https://www.tfrrs.org/athletes/" + ID + "/" 
+        if school:
+            self.url += school + "/"
+        if name:
+            self.url += name.replace(" ", "_") + ".html"
+        self.retrieve()
 
-        #Get athlete info
-        info = getAthleteInfo(HTML)
-        #print(info)
+    def retrieve(self):
+        #Make the request
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.102 Safari/537.36"}        
+        response = requests.get(self.url, headers=headers)    
+        
+        #Handle the response
+        if response.status_code < 300 and response.status_code >= 200:
+            #panda's read_html doesn't accept percent colspan arguments
+            self.HTML = response.text.replace("colspan=\"100%\"", "colspan=\"3\"")
 
-        #Get Personal Records
-        PRs = getPersonalRecords(HTML)
-        #print(PRs)
+        else:
+            self.HTML = ""
+            raise Exception("Could not retrieve", response.status_code)
 
+    def parse(self):
+        if self.HTML:
+            #Get athlete info
+            info = self.getAthleteInfo()
+            #print(info)
+
+            #Get Personal Records
+            PRs = self.getPersonalRecords()
+            #print(PRs)
+
+            #Get meet information
+            dfs = pd.read_html(self.HTML, header=0)
+            df = self.parseDataframe(dfs[1])
+            #print(df)
+
+        else:
+            raise Exception("No HTML loaded")
+
+    def getAthleteInfo(self):
+        #Use beautifulsoup to find the proper section and extract the text
+        soup = BeautifulSoup(self.HTML, "html5lib")
+        athleteInfo = soup.find('div', class_="panel-heading").get_text().replace("\n", "").strip()
+        athleteInfo = ' '.join(athleteInfo.split())
+        
+        #Format the text into a usable list
+        athleteInfo = athleteInfo.split()
+        athleteInfo[0] = athleteInfo[0] + " " + athleteInfo[1]
+        grade, year = athleteInfo[2].split("-")
+        athleteInfo[1] = grade[1:3]
+        athleteInfo[2] = year[0:1]
+        
+        return athleteInfo
+
+    def getPersonalRecords(self):
+        return "Hi"
+
+    def parseDates(self, Date):
+        if "-" in Date:
+            days = Date[Date.index(" ")+1:Date.index(",")]
+            Date = Date.replace(days, "").split(",")
+            return Date[0] + days.split("-")[0] + "," + Date[1], Date[0] + days.split("-")[1] + "," + Date[1]
+        else:
+            return Date, Date
+
+    def parseDataframe(self, df):
         #Get meet information
-        dfs = pd.read_html(HTML, header=0)
-        #parseDataframe(dfs[1])
+        Meet, Date = df.columns[0].split("  ")
+        startDate, endDate = self.parseDates(Date)
 
-    else:
-        print("Error {} occured".format(response.status_code))
+        #Add a column and rename columns    
+        df = pd.concat([df, pd.DataFrame(arange(df.shape[0]).transpose())], axis=1)
+        df.columns = ["Event", "Mark", "Place", "Round"]
 
-def getAthleteInfo(HTML):
-    #Use beautifulsoup to find the proper section and extract the text
-    soup = BeautifulSoup(HTML, "html5lib")
-    athleteInfo = soup.find('div', class_="panel-heading").get_text().replace("\n", "").strip()
-    athleteInfo = ' '.join(athleteInfo.split())
-    
-    #Format the text into a usable list
-    athleteInfo = athleteInfo.split()
-    athleteInfo[0] = athleteInfo[0] + " " + athleteInfo[1]
-    grade, year = athleteInfo[2].split("-")
-    athleteInfo[1] = grade[1:3]
-    athleteInfo[2] = year[0:1]
-    
-    return athleteInfo
-
-def getPersonalRecords(HTML):
-    return "Hi"
-
-def parseDates(Date):
-    if "-" in Date:
-        days = Date[Date.index(" ")+1:Date.index(",")]
-        Date = Date.replace(days, "").split(",")
-        return Date[0] + days.split("-")[0] + "," + Date[1], Date[0] + days.split("-")[1] + "," + Date[1]
-    else:
-        return Date, Date
-
-def parseDataframe(df):
-    #Get meet information
-    Meet, Date = df.columns[0].split("  ")
-    startDate, endDate = parseDates(Date)
-
-    #Add a column and rename columns    
-    df = pd.concat([df, pd.DataFrame(arange(df.shape[0]).transpose())], axis=1)
-    df.columns = ["Event", "Mark", "Place", "Round"]
-
-    #Settle columns
-    df["Round"] = ["F" if "(F)" in row else "P" for row in df["Place"]]
-    df["Place"] = [row[0:len(row)-4] for row in df["Place"]]
-    df["Mark"]  = [row[0:row.index("m")] if "m" in row else row for row in df["Mark"]]
-    
-    #print(df)
+        #Settle columns
+        df["Round"] = ["F" if "(F)" in row else "P" for row in df["Place"]]
+        df["Place"] = [row[0:len(row)-4] for row in df["Place"]]
+        df["Mark"]  = [row[0:row.index("m")] if "m" in row else row for row in df["Mark"]]
+        
+        return df
 
 if __name__ == '__main__':
-    main()
+    Test = TfrrsApi("6092422", "RPI", "Mark Shapiro")
+    Test.parse()
