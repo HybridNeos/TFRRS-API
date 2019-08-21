ea10f9f0a5a85af0330b43b26ef0463204d7b497
HybridNeos <mark3198@gmail.com>
2019-08-21 18:20:15 -0400

    True Key used. Event format given
    Meets now have their ID use for their key in the JSON. Also, Added an item Result Format: Event: [x, y, z, ...] so someone can see how I store results

--- TfrrsApi.py
+++ TfrrsApi.py
@@ -7,6 +7,7 @@ from pprint import PrettyPrinter
 from numpy import empty
 from bs4 import BeautifulSoup
 
+
 class TfrrsApi:
     def __init__(self, ID, school="", name=""):
         self.url = "https://www.tfrrs.org/athletes/" + ID + "/"
@@ -17,14 +18,16 @@ class TfrrsApi:
         self.retrieve()
 
     def retrieve(self):
-        #Make the request
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.102 Safari/537.36"}
+        # Make the request
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.102 Safari/537.36"
+        }
         response = requests.get(self.url, headers=headers)
 
-        #Handle the response
+        # Handle the response
         if response.status_code < 300 and response.status_code >= 200:
-            #panda's read_html doesn't accept percent colspan arguments
-            self.HTML = response.text.replace("colspan=\"100%\"", "colspan=\"3\"")
+            # panda's read_html doesn't accept percent colspan arguments
+            self.HTML = response.text.replace('colspan="100%"', 'colspan="3"')
 
         else:
             self.HTML = None
@@ -32,47 +35,122 @@ class TfrrsApi:
 
     def parse(self):
         if self.HTML:
-            #Setup
+            # Setup
             dfs = pd.read_html(self.HTML)
             self.data = OrderedDict()
 
-            #Get athlete info
+            # Get athlete info
             self.getAthleteInfo()
 
-            #PRs
+            # PRs
             self.parsePersonalRecords(dfs[0])
-            
-            #Meet results
-            # More than meet results are read in. Use regex to read in only meet results (they have a date while others don't)
-            self.data["Meet Results"] = OrderedDict()
-            #self.dateRegex = "[A-Z][a-z]{2} \d{1,2},|[A-z][a-z]{2} \d{1,2}-\d{1,2},|\d\d/\d\d -|[a-zA-Z]{3}"
 
-            self.dateRegex = "[A-Z][a-z]{2} \d{1,2}(-\d{1,2}){0,1},"
-            #firstNonResult = [True if (re.search(self.dateRegex, df.columns[0])) else False for df in dfs[1:]].index(False) + 1
-            firstNonResult = 2
-            for df in dfs[1:firstNonResult]:
-                self.parseMeetResult(df)
+            # Meet results
+            self.parseMeetResults(dfs[1:])
 
-            #Return
+            # Return
             return json.dumps(self.data, indent=4)
 
         else:
             raise Exception("No HTML loaded. Retry constructor with a different ID")
 
-    def getAthleteInfo(self):
-        #Use beautifulsoup to find the proper section and extract the text
+    def getMeetIds(self):
         soup = BeautifulSoup(self.HTML, "html5lib")
-        athleteInfo = soup.find('div', class_="panel-heading").get_text().replace("\n", "").strip()
-        athleteInfo = ' '.join(athleteInfo.split())
+        links = soup.find_all('a')
+        IDs = []
 
-        #Format the text into a usable list
+        # Pull the meet ID from href URLs
+        for link in links:
+            if re.search("href=\"//www.tfrrs.org/results/\d{5,6}/\w+_", str(link)):
+                link = str(link)
+                idStart = re.search("/results/\d{5,6}/", link).start() + len("/results/")
+                IDs += [link[idStart:idStart+5]]
+
+        return IDs
+
+    def parseOneMeet(self, df, ID):
+        # Get meet name and date
+        dateStart = re.search(self.dateRegex, df.columns[0]).start()
+        Meet = df.columns[0][:dateStart].rstrip()
+        Date = df.columns[0][dateStart:]
+        startDate, endDate = self.parseDates(Date)
+
+        # JSON the meet info
+        meetInfo = OrderedDict()
+        meetInfo["Meet Name"] = Meet
+        meetInfo["Start Date"] = startDate
+        meetInfo["End Date"] = endDate
+
+        # Add a column and rename columns
+        df = pd.concat(
+            [df, pd.DataFrame(empty([df.shape[0], 1], dtype=object))], axis=1
+        )
+        df.columns = ["Event", "Mark", "Place", "Round"]
+
+        # Settle columns
+        df["Mark"] = df["Mark"].apply(
+            lambda mark: self.parseEventMark(
+                df["Event"][df[df["Mark"] == mark].index.item()], mark
+            )
+        )
+        df["Place"] = df["Place"].fillna("N/A")
+        df["Round"] = [
+            "F" if "(F)" in row else ("P" if "(P)" in row else "N/A")
+            for row in df["Place"]
+        ]
+        df["Place"] = [
+            row if row == "N/A" else row[0 : len(row) - 4] for row in df["Place"]
+        ]
+        df.set_index("Event", inplace=True)
+
+        # JSON to meet results
+        meetInfo["Results"] = OrderedDict()
+        for i in range(0, df.shape[0]):
+            meetInfo["Results"][df.index[i]] = df.iloc[i, :].to_list()
+
+        # add into data
+        self.data["Meet Results"][ID] = meetInfo
+
+    def parseMeetResults(self, dfs):
+        # Setup
+        self.data["Result Format"] = "Event: [Mark, Place, Round]"
+        self.data["Meet Results"] = OrderedDict()
+
+        # Since more than meet results are read in, use regex to determine when they stop
+        self.dateRegex = "[A-Z][a-z]{2} \d{1,2}(-\d{1,2}){0,1},"
+        firstNonResult = [
+            True if (re.search(self.dateRegex, df.columns[0])) else False
+            for df in dfs[1:]
+        ].index(False) + 1
+
+        # Get the meet IDs ahead of time and pass that to the JSON creating function
+        IDs = self.getMeetIds()
+        i = 0
+
+        # Loop
+        for df in dfs[1:firstNonResult]:
+            self.parseOneMeet(df, IDs[i])
+            i += 1
+
+    def getAthleteInfo(self):
+        # Use beautifulsoup to find the proper section and extract the text
+        soup = BeautifulSoup(self.HTML, "html5lib")
+        athleteInfo = (
+            soup.find("div", class_="panel-heading")
+            .get_text()
+            .replace("\n", "")
+            .strip()
+        )
+        athleteInfo = " ".join(athleteInfo.split())
+
+        # Format the text into a usable list
         athleteInfo = athleteInfo.split()
         athleteInfo[0] = athleteInfo[0] + " " + athleteInfo[1]
         grade, year = athleteInfo[2].split("-")
         athleteInfo[1] = grade[1:3]
         athleteInfo[2] = year[0:1]
 
-        #Put it into data
+        # Put it into data
         self.data["Name"] = athleteInfo[0]
         self.data["Grade"] = athleteInfo[1]
         self.data["Year"] = athleteInfo[2]
@@ -80,95 +158,87 @@ class TfrrsApi:
 
     def parseDates(self, Date):
         if "/" in Date:
+
             def chunkToFormat(chunk):
                 month, day = chunk.split("/")
-                numToMonth = {"01":"Jan", "02":"Feb", "03":"Mar", "04":"Apr", "05":"May", "06":"Jun", "07":"Jul", "08":"Aug", "09":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"}
+                numToMonth = {
+                    "01": "Jan",
+                    "02": "Feb",
+                    "03": "Mar",
+                    "04": "Apr",
+                    "05": "May",
+                    "06": "Jun",
+                    "07": "Jul",
+                    "08": "Aug",
+                    "09": "Sep",
+                    "10": "Oct",
+                    "11": "Nov",
+                    "12": "Dec",
+                }
                 month = numToMonth[month]
                 return month + " " + day
 
             dashIndex = Date.index("-")
             year = Date[-4:]
-            chunk = Date[:dashIndex-1]
-            return chunkToFormat(chunk)+", "+year, Date[dashIndex+2:]
+            chunk = Date[: dashIndex - 1]
+            return chunkToFormat(chunk) + ", " + year, Date[dashIndex + 2 :]
 
         elif "-" in Date:
-            Month = Date[:Date.index(" ")]
+            Month = Date[: Date.index(" ")]
             Year = Date[-4:]
             Days = Date.split(" ")[1].replace(",", "").split("-")
-            return Month+" "+Days[0]+", "+Year, Month+" "+Days[1]+", "+Year
+            return (
+                Month + " " + Days[0] + ", " + Year,
+                Month + " " + Days[1] + ", " + Year,
+            )
 
         else:
             return Date, Date
 
     def parseEventMark(self, eventType, mark):
-        if (eventType in ["SP", "DT", "HT", "WT", "JT"]):
+        if eventType in ["SP", "DT", "HT", "WT", "JT"]:
             return mark if mark.isalpha() else mark.split(" ")[0]
         else:
             return mark
 
     def parsePersonalRecords(self, df):
-        #Create the np array to fill in
-        numLeft = sum(pd.notnull(df.iloc[:,0]))
-        numRight = sum(pd.notnull(df.iloc[:,2]))
+        # Create the np array to fill in
+        numLeft = sum(pd.notnull(df.iloc[:, 0]))
+        numRight = sum(pd.notnull(df.iloc[:, 2]))
         numEvents = numLeft + numRight
         PRs = empty([numEvents, 2], dtype=object)
 
-        #Fill in the array
+        # Fill in the array
         for i in range(0, df.shape[0]):
             PRs[i, 0] = df.iloc[i, 0]
             PRs[i, 1] = df.iloc[i, 1]
 
             if pd.notnull(df.iloc[i, 2]):
-                PRs[i+numLeft, 0] = df.iloc[i, 2]
-                PRs[i+numLeft, 1] = df.iloc[i, 3]
+                PRs[i + numLeft, 0] = df.iloc[i, 2]
+                PRs[i + numLeft, 1] = df.iloc[i, 3]
 
-        #Convert to dataframe
+        # Convert to dataframe
         PRs = pd.DataFrame(PRs)
         PRs.columns = ["Event", "Mark"]
 
-        #Clean up the dataframe
-        PRs["Mark"] = PRs["Mark"].apply(lambda mark: self.parseEventMark(PRs["Event"][PRs[PRs["Mark"]==mark].index.item()], mark))
+        # Clean up the dataframe
+        PRs["Mark"] = PRs["Mark"].apply(
+            lambda mark: self.parseEventMark(
+                PRs["Event"][PRs[PRs["Mark"] == mark].index.item()], mark
+            )
+        )
         PRs.set_index("Event", inplace=True)
 
-        #Put it into data
-        #  ["Mark"] used since column name persists
+        # Put it into data
+        #   ["Mark"] used since column name persists
         self.data["College Bests"] = PRs.to_dict()["Mark"]
 
-    def parseMeetResult(self, df):
-        #Get meet name and date
-        dateStart = re.search(self.dateRegex, df.columns[0]).start()
-        Meet = df.columns[0][:dateStart].rstrip()
-        Date = df.columns[0][dateStart:]
-        startDate, endDate = self.parseDates(Date)
+    
 
-        #JSON the meet info
-        meetInfo = OrderedDict()
-        meetInfo["Meet Name"] = Meet
-        meetInfo["Start Date"] = startDate
-        meetInfo["End Date"] = endDate
 
-        #Add a column and rename columns
-        df = pd.concat([df, pd.DataFrame(empty([df.shape[0], 1], dtype=object))], axis=1)
-        df.columns = ["Event", "Mark", "Place", "Round"]
-
-        #Settle columns
-        df["Mark"] = df["Mark"].apply(lambda mark: self.parseEventMark(df["Event"][df[df["Mark"]==mark].index.item()], mark))
-        df["Place"] = df["Place"].fillna("N/A")
-        df["Round"] = ["F" if "(F)" in row else ( "P" if "(P)" in row else "N/A") for row in df["Place"]]
-        df["Place"] = [row if row == "N/A" else row[0:len(row)-4] for row in df["Place"]]
-        df.set_index("Event", inplace=True)
-
-        #JSON to meet results
-        meetInfo["Results"] = OrderedDict()
-        for i in range(0, df.shape[0]):
-            meetInfo["Results"][df.index[i]] = df.iloc[i,:].to_list()
-
-        #add into data
-        self.data["Meet Results"][startDate] = meetInfo
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     Test = TfrrsApi("6092422", "RPI", "Mark Shapiro")
-    #Test = TfrrsApi("6092256", "RPI", "Patrick Butler")
-    #Test = TfrrsApi("5462222", "LORAS", "Audrey Miller")
-    #out = Test.parse()
-    #print(out)
+    # Test = TfrrsApi("6092256", "RPI", "Patrick Butler")
+    # Test = TfrrsApi("5462222", "LORAS", "Audrey Miller")
+    out = Test.parse()
+    print(out)
