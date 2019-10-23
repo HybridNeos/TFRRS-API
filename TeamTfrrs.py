a89f99fe0ddab3c7fabda83c41f2ec3fb94ff155
Mark Shapiro <mark3198@gmail.com>
2019-10-23 12:20:37 -0400

    Appended meet IDs to Latest Results. Added support for XC meets. Going to update individual to have XC results.

--- TeamTfrrs.py
+++ TeamTfrrs.py
@@ -6,10 +6,18 @@ from collections import OrderedDict
 from bs4 import BeautifulSoup
 from AthleteTfrrs import AthleteTfrrs
 
+
 class Team:
     def __init__(self, State, Gender, Name):
         # Construct the url and make the request
-        url_stub = "http://www.tfrrs.org/teams/"+State+"_college_"+Gender.lower()+"_"+Name
+        url_stub = (
+            "http://www.tfrrs.org/teams/"
+            + State
+            + "_college_"
+            + Gender.lower()
+            + "_"
+            + Name
+        )
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.102 Safari/537.36"
         }
@@ -33,6 +41,7 @@ class Team:
 
             # Get Athlete and Meet IDs to append to tables later
             self.AthleteIDs = self.getAthleteIDs()
+            self.MeetIDs = self.getMeetIds()
 
             # Get the basic three tables
             self.getMainInformation(dfs)
@@ -52,11 +61,10 @@ class Team:
         for link in links:
             link = str(link)
             if "//www.tfrrs.org/athletes/" in link:
-                link = link[34:link.index(".html")]
-                ID = link.split("/")[0]
-                name = link.split("/")[2]
+                link = link[34 : link.index(".html")]
+                ID, school, name = link.split("/")
                 first, last = name.split("_")
-                name = last+", "+first
+                name = last + ", " + first
                 tempIDs[name] = ID
 
         # sort it since we had duplicates from top marks
@@ -66,27 +74,56 @@ class Team:
 
         return IDs
 
+    #MERGE Into meetIds, AthleteIds = self.getIDs() to reduce double soup.findl_all("a")
+    # not working
+    # breaks at Bates, RPI, MIT, Tufts
+    def getMeetIds(self):
+        links = self.soup.find_all("a")
+        IDs = []
+
+        # Pull the meet ID from href URLs
+        for link in links:
+            if re.search('href="//www.tfrrs.org/results/(xc/){0,1}\d{5,6}/\w+(,|_)', str(link)):
+                link = str(link)
+                idStart = re.search("/results/(xc/){0,1}\d{5,6}/", link).start() + len(
+                    "/results/"
+                )
+                IDs += [link[idStart+3 : idStart+8]] if "xc" in link else [link[idStart : idStart + 5]]
+
+        return IDs
+
     def getMainInformation(self, dfs):
         # Pull out the tables
         TopMarks = dfs[0]
         Roster = dfs[1]
         LatestResults = dfs[2]
 
-        #####IDS are wrong
         # Append Athlete ID to TopMarks
-        MarkIDs = pd.Series([self.AthleteIDs[name] if name in self.AthleteIDs.keys() else None for name in TopMarks["ATHLETE/SQUAD"]])
+        MarkIDs = pd.Series(
+            [
+                self.AthleteIDs[name] if name in self.AthleteIDs.keys() else None
+                for name in TopMarks["ATHLETE/SQUAD"]
+            ]
+        )
         TopMarks = pd.concat((TopMarks, MarkIDs.rename("Athlete ID")), axis=1)
-        print(TopMarks.head())
 
         # Append Athlete ID to Roster
-        RosterIDs = pd.Series([self.AthleteIDs[name] for name in Roster["NAME"].to_list()])
+        RosterIDs = pd.Series(
+            [self.AthleteIDs[name] for name in Roster["NAME"].to_list()]
+        )
         Roster = pd.concat([Roster, RosterIDs.rename("Athlete ID")], axis=1)
 
         # TODO: Append MeetId to LatestResults
+        # off by 1
+        MeetIDs = pd.Series(self.MeetIDs)
+        LatestResults = pd.concat((LatestResults, MeetIDs.rename("Meet ID")), axis=1)
+        print(LatestResults)
+
 
 def TeamTfrrs(State, Gender, Name):
     TeamResults = Team(State, Gender, Name)
     return TeamResults.parse()
+
 
 if __name__ == "__main__":
     Test = TeamTfrrs("NY", "M", "RPI")
