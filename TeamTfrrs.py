fd40f6be861bdd33a4279a72250be82811820af8
Mark Shapiro <mark3198@gmail.com>
2019-10-23 11:40:43 -0400

    Added Athlete ID to Roster and Top Marks.

--- TeamTfrrs.py
+++ TeamTfrrs.py
@@ -30,10 +30,59 @@ class Team:
             dfs = pd.read_html(self.HTML)
             self.data = OrderedDict()
             self.soup = BeautifulSoup(self.HTML, "html5lib")
-            
+
+            # Get Athlete and Meet IDs to append to tables later
+            self.AthleteIDs = self.getAthleteIDs()
+
+            # Get the basic three tables
+            self.getMainInformation(dfs)
+            # TODO: add to internal df
+
+            # TODO: Get region, more teams, season
 
         else:
             raise Exception("No HTML loaded. Retry with a different information")
+
+    def getAthleteIDs(self):
+        # TODO: Append Athlete ID(s) to Top Marks
+        links = self.soup.find_all("a")
+        tempIDs = {}
+
+        # Pull the athlete ID from href URLs
+        for link in links:
+            link = str(link)
+            if "//www.tfrrs.org/athletes/" in link:
+                link = link[34:link.index(".html")]
+                ID = link.split("/")[0]
+                name = link.split("/")[2]
+                first, last = name.split("_")
+                name = last+", "+first
+                tempIDs[name] = ID
+
+        # sort it since we had duplicates from top marks
+        IDs = {}
+        for item in sorted(tempIDs.items()):
+            IDs[item[0]] = item[1]
+
+        return IDs
+
+    def getMainInformation(self, dfs):
+        # Pull out the tables
+        TopMarks = dfs[0]
+        Roster = dfs[1]
+        LatestResults = dfs[2]
+
+        #####IDS are wrong
+        # Append Athlete ID to TopMarks
+        MarkIDs = pd.Series([self.AthleteIDs[name] if name in self.AthleteIDs.keys() else None for name in TopMarks["ATHLETE/SQUAD"]])
+        TopMarks = pd.concat((TopMarks, MarkIDs.rename("Athlete ID")), axis=1)
+        print(TopMarks.head())
+
+        # Append Athlete ID to Roster
+        RosterIDs = pd.Series([self.AthleteIDs[name] for name in Roster["NAME"].to_list()])
+        Roster = pd.concat([Roster, RosterIDs.rename("Athlete ID")], axis=1)
+
+        # TODO: Append MeetId to LatestResults
 
 def TeamTfrrs(State, Gender, Name):
     TeamResults = Team(State, Gender, Name)
