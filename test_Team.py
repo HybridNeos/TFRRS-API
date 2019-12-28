092135a2e41c917e9c9a21226178aa1738f18bb5
Mark Shapiro <mark3198@gmail.com>
2019-12-28 13:32:52 -0500

    Test renames

---
+++ test_Team.py
@@ -0,0 +1,9 @@
+from TeamTfrrs import Team
+import unittest
+from concurrent.futures import ThreadPoolExecutor
+
+class TestTfrrsApi(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
